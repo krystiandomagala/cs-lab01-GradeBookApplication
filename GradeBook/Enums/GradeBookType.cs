3c7a0f02038ec4762bebd6be865eeaf0a981a346
Krystian Domagala <krystian.domagala@microsoft.wsei.edu.pl>
2022-03-03 13:03:02 +0100

    Creating The GradeBookType Enum, Add Type property

---
+++ GradeBook/Enums/GradeBookType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.Enums
+{
+    public enum GradeBookType // Creating The GradeBookType Enum
+    {
+        Standard, 
+        Ranked,
+        ESNU,
+        OneToFour,
+        SixPoint
+    }
+}
--- GradeBook/Program.cs
+++ GradeBook/Program.cs
@@ -11,7 +11,7 @@ namespace GradeBook
             Console.WriteLine("# Welcome to GradeBook! #");
             Console.WriteLine("#=======================#");
 
-            StartingUserInterface.CommandLoop();
+            StartingUserInterface.CommandLoop(); 
             
             Console.WriteLine("Thank you for using GradeBook!");
             Console.WriteLine("Have a nice day!");
